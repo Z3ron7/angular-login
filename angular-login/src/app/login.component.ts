be60450bf5f5dba8f2b9fdee96839978971085d7
Jeahz Carbajosa <zoren.panilagao1@gmail.com>
2024-08-05 00:56:08 +0800

    updates

---
+++ angular-login/src/app/login.component.ts
@@ -0,0 +1,57 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  title = 'angular-login';
+  hide = signal(true);
+  submitted = false;
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.form.markAllAsTouched();
+
+    if (this.form.valid) {
+      console.log('Form Submitted', this.form.value);
+    } else {
+      console.log('Form is invalid');
+    }
+
+    this.cdr.detectChanges();
+  }
+}
